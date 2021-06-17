365dd7ad36579e391d7364c9a0c80fa6d5cb08b7
Vinicius Castro <vinycastros@gmail.com>
2021-06-17 19:58:23 -0300

    feat: Imclusão da tela home no menu com as movimentações

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,18 @@
+
+import 'package:ReggitreApp/screens/menu/screen/InicialPage.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'screens/screens.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
-  runApp(MyApp());
-}
+main() {
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Reggistre',
-      theme: ThemeData(
-        textTheme:
-            GoogleFonts.josefinSansTextTheme(Theme.of(context).textTheme),
-        primarySwatch: Colors.green,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => LoginScreen(),
-        'ForgotPassword': (context) => ForgotPassword(),
-        'CreateNewAccount': (context) => CreateNewAccount(),
-      },
-    );
-  }
+
+  initializeDateFormatting().then((_){
+    runApp(MaterialApp(
+      home: InicialPage(),
+      debugShowCheckedModeBanner: false,
+
+    ));
+  });
+
 }
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,3 +1,5 @@
 export 'authentication/login-screen.dart';
 export 'authentication/forgot-password.dart';
 export 'authentication/create-new-account.dart';
+export 'menu/screen/HomePage.dart';
+export 'menu/screen/InicialPage.dart';
